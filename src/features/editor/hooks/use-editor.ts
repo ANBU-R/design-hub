d06537f13451bc2104d6bf3c43ea1b145d2d335a
Anbarasan R <51128356+ANBU-R@users.noreply.github.com>
2025-02-18 15:29:19 +0530

    Add workspace slide

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -9,6 +9,19 @@ export const useEditor = () => {
       initialCanvas: fabric.Canvas;
       initialContainer: HTMLDivElement;
     }) => {
+    
+      const initialWorkspace = new fabric.Rect({
+        width: 900,
+        height: 1200,
+        name: "slide",
+        fill: "white",
+        selectable: false,
+        hasControls: false,
+        shadow: new fabric.Shadow({
+          color: "rgba(0,0,0,0.8)",
+          blur: 5,
+        }),
+      });
       initialCanvas.setWidth(initialContainer.offsetWidth);
       initialCanvas.setHeight(initialContainer.offsetHeight);
     },
